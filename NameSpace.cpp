b97ad668cce965ce4d9b62f1f44d9daa5b4d968f
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-18 23:26:32 +0900

    Add files via upload

---
+++ NameSpace.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//선언 먼저
+namespace BestComImp1 {
+	void SimpleFunc(void);
+}
+
+namespace ProgComImp1 {
+	void SimpleFunc(void);
+}
+
+int main(void) {
+
+	BestComImp1::SimpleFunc();
+	ProgComImp1::SimpleFunc();
+
+	return 0;
+}
+
+void BestComImp1::SimpleFunc(void) {
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgComImp1::SimpleFunc(void) {
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
