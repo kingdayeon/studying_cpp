01c00d7a2bcc86e3e6427189c35e829e9949995e
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-19 11:47:39 +0900

    Add files via upload

---
+++ Struct_in_Func.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Point {
+	int xpos;
+	int ypos;
+
+	void MovePos(int x, int y) {
+		xpos = xpos + x;
+		ypos = ypos + y;
+	}
+
+	void AddPoint(const Point& pos) {
+		xpos = xpos + pos.xpos;
+		ypos = ypos + pos.ypos;
+	}
+
+	void ShowPosition() {
+		cout <<"["<< xpos <<" ,"<< ypos << "]"<<endl;
+	}
+
+}; //여기는 ; 달아줘
+
+int main(void) {
+
+	Point pos1 = { 12,4 };
+	Point pos2 = { 20,30 };
+
+	pos1.MovePos(-7, 10);
+	pos1.ShowPosition();
+
+	pos1.AddPoint(pos2);
+	pos1.ShowPosition();
+
+	return 0;
+}
