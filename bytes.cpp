3865a354a41df0f69d349ebb21f9b27970bf75bb
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-10-11 09:14:03 +0900

    Create bytes.cpp

---
+++ bytes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "char: " << sizeof(char) << "Bytes" << endl;
+   cout << "unsigned char: " << sizeof(unsigned char) << "Bytes" << endl;
+   cout << "signed char: " << sizeof(signed char) << "Bytes" << endl;
+
+   cout << "int: " << sizeof(int) << "Bytes" << endl;
+   cout << "unsigned int: " << sizeof(unsigned int) << "Bytes" << endl;
+   cout << "signed int: " << sizeof(signed int) << "Bytes" << endl;
+   
+   cout << "float: " << sizeof(float) << "Bytes" << endl;
+   cout << "double: " << sizeof(double) << "Bytes" << endl;
+   cout << "bool: " << sizeof(bool) << "Bytes" << endl;
+}  
