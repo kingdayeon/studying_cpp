5b4ab951fe7bca6e7ade8643d62d043fd5940729
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-10-15 21:46:58 +0900

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,11 @@
+int int1, int2, sum;
+   cout << "Enter the integer1 : " << endl;
+   cin >> int1; //scanf
+   cout << "Enter the integer2 : " << endl;
+   cin >> int2;
+
+   sum = int1 + int2;
+   cout << "Sum is : " << sum << endl;
+
+   int remainder = int1 % int2;
+   cout << "Remainder is : " << remainder << endl;
